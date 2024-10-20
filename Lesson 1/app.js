e601d952c5d1a797f1ceb4116349c21f35d10465
Fidanmusali <fidanmusali05@gmail.com>
2024-10-20 22:11:46 +0400

    1

---
+++ Lesson 1/app.js
@@ -0,0 +1,22 @@
+// import readline from "readline";
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// const username = "admin";
+// const password = "admin123";
+
+// rl.question("istifadeci adi: ", (u) => {
+//     rl.question("sifre: ", (s) => {
+//         if (u === username && s === password) {
+//             console.log("daxil oldunuz!");
+//         } else {
+//             console.log("xeta!");
+//         }
+//         process.exit();
+//     });
+// });
+
+
---
+++ Lesson 1/data.tsx
@@ -0,0 +1,1 @@
+"FIdan Musali"
